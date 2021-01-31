bb4013a603ae07b9bc1087adaccc3f763f9d8a2c
cdfmlr <cdfmlr@163.com>
2021-01-31 12:16:29 +0800

    Initial Commit

---
+++ Courser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Courser
+//
+//  Created by c on 2021/1/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Courser/CourserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourserApp.swift
+//  Courser
+//
+//  Created by c on 2021/1/31.
+//
+
+import SwiftUI
+
+@main
+struct CourserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
