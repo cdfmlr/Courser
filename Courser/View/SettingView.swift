ba4d23d627365327ccc2ba03e57b50445b47fb85
cdfmlr <cdfmlr@163.com>
2021-02-25 14:10:49 +0800

    feat(View): implement a basic SettingView

--- Courser/View/SettingView.swift
+++ Courser/View/SettingView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct SettingView: View {
+    @State var model: Settings
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            accountSection
+//            actionSection  // cacheCleaning 什么的
+        }
+    }
+    
+    var accountSection: some View {
+        Section(header: Text("教务账户")) {
+            TextField("学号", text: $model.account.sid)
+            TextField("密码", text: $model.account.password)
+            Button("登录测试") {
+                print("login")
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+        
     }
 }
 
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingView()
+        SettingView(model: Settings())
     }
 }
